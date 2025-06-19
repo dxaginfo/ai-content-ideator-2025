379f64906f1ca5548caefdde37d1c4544706d166
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-19 09:37:39 -0300

    Add models index file

---
+++ backend/src/models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User');
+const ContentIdea = require('./ContentIdea');
+const TrendData = require('./TrendData');
+
+module.exports = {
+  User,
+  ContentIdea,
+  TrendData
+};
